fafcc2a7978de4464ac125453abc2a2aaddceb12
huseyyinsahin <huseysahin19@outlook.com>
2025-02-07 22:51:05 +0300

    index.js

--- index.js
+++ index.js
@@ -2,38 +2,51 @@
 
 const express = require("express");
 const app = express();
-
-const { dbConnection, mongoose } = require("./src/config/dbConnection");
+const { dbConnection } = require("./src/configs/dbConnection");
+const cors = require("cors");
 
 /* ------------------------------------------------------- */
-require("express-async-errors");
 
 require("dotenv").config();
+require("express-async-errors");
+
 const PORT = process.env?.PORT || 8000;
+
 /* ------------------------------------------------------- */
 
 dbConnection();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
 
-/* ------------------------------------------------------- */
-
 app.use(require("./src/middlewares/authentication"));
-app.use(require("./src/middlewares/queryHandler"));
+app.use(require("./src/middlewares/query"));
 
 /* ------------------------------------------------------- */
 
-app.use(require("./src/routes/index"));
-
-app.all("*", async (req, res) => {
-  res.status(404).send({
-    error: true,
-    message: "Route not available",
+app.all("/", (req, res) => {
+  res.send({
+    error: false,
+    message: "Welcome to Infinite BLOG",
+    documents: {
+      swagger: "/documents/swagger",
+      redoc: "/documents/redoc",
+      json: "/documents/json",
+    },
   });
 });
+
+app.use(require("./src/routes/index"));
 
 app.use(require("./src/middlewares/errorHandler"));
 
 /* ------------------------------------------------------- */
 
-app.listen(PORT);
+app.listen(PORT, () => console.log(`${PORT}`));
+
+require("./src/helpers/sync")();
