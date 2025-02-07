96bcb7c1f97abcf327648d14ccdbd5908b45fc39
huseyyinsahin <huseysahin19@outlook.com>
2025-02-07 22:51:25 +0300

    middlewares

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const Token = require("../models/token");
+
+module.exports = async (req, res, next) => {
+  const auth = req.headers?.authorization || null;
+  const tokenKey = auth ? auth.split(" ") : null;
+
+  req.user = null;
+
+  if (tokenKey && tokenKey[0] == "Token") {
+    const tokenData = await Token.findOne({ token: tokenKey[1] }).populate(
+      "userId"
+    );
+    if (tokenData) req.user = tokenData.userId;
+  }
+  next();
+};
