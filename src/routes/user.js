fcfe1fb11a745c0910582ac556cd39ae653b3e9c
huseyyinsahin <huseysahin19@outlook.com>
2025-02-12 22:02:42 +0300

    user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,6 +7,10 @@ const { isLogin } = require("../middlewares/permissions");
 
 router.route("/").post(user.create);
 
-router.route("/:id").put(isLogin, user.update).patch(isLogin, user.update);
+router
+  .route("/:id")
+  .get(isLogin, user.read)
+  .put(isLogin, user.update)
+  .patch(isLogin, user.update);
 
 module.exports = router;
